7143be824a0ef9ef734410c262cba9d5db12d164
Carl A. Adams <carl.adams@planet.com>
2021-04-07 17:08:29 -0700

    Adding a Dockerfile for packaging up a CLI.
    To build the CLI:
        make docker-build-runtime
    
    To run unit tests on this build:
        make docker-run-utest

--- Makefile
+++ Makefile
@@ -1,29 +1,33 @@
 GH_PAGES_SOURCES = planet docs Makefile
 
-check:
+default: help
+
+check: ## Run check commands locally
 	py.test --doctest-modules planet tests
 	flake8 planet tests
 
-coverage:
+coverage: ## Run coverage locally
 	py.test --doctest-modules --cov planet --cov-report=html:htmlcov tests planet/api
 
-pex:
+pex: ## Run pex locally
 	# disable-cache seemed required or the older version would be used
 	pex . -o dist/planet -e planet.scripts:main --disable-cache
 
-html-docs:
+html-docs: ## Generate HTML documents locally
 	python docs/source/generate.py
 	$(MAKE) -C docs clean html
 
-auto-docs:
+auto-docs: ## Autobuild doc locally with sphinx
 	which sphinx-autobuild || ( echo 'PLEASE INSTALL sphinx-autobuild: pip install sphinx-autobuild'; exit 1 )
 	sphinx-autobuild --watch planet docs/source docs/build/html
 
+
+docs-zip: ## Build a zip file of HTML documents locally
 docs-zip: html-docs
 	cp -a docs/build/html planet-docs
 	zip -m -r dist/planet-docs.zip planet-docs
 
-gh-docs:
+gh-docs: ## Build GitHub documents locally
 	git checkout gh-pages
 	rm -rf *
 	git checkout master $(GH_PAGES_SOURCES)
@@ -34,7 +38,7 @@ gh-docs:
 	git add -A
 	git commit -m "gh-pages for `git log master -1 --oneline`"
 
-release:
+release: ## Perform release actions
 	@[ $(TAG) ] || exit 1
 	@[ $(BODY) ] || exit 1
 	@sed -i -e "s/__version__ =.*/__version__ = '$(TAG)'/" planet/api/__version__.py
@@ -48,5 +52,18 @@ release:
 	git push origin
 	make DRAFT=false release-gh
 
+release-gh: ## Perform GitHub release actions
 release-gh: check pex docs-zip
 	TAG="$(TAG)" BODY="$(BODY)" DRAFT="$(DRAFT)" ./gh-release
+
+
+DOCKER_NAME:=planet-client
+DOCKER_FQNAME:=planetlabs/planet-client
+# TODO: get the version from planet/api/__version__.py for the tag
+DOCKER_VERSION=$(if $(strip $(TAG)),$(strip $(TAG)),0.0.0-Dev0)
+
+runtime__DOCKER_RUN_OPTS=-ti
+runtime__DOCKER_RUN_ARGS=--help
+
+include Makefile-DockerRules.mk
+
---
+++ Makefile-DockerRules.mk
@@ -0,0 +1,45 @@
+##
+## Docker pattern targets. These pattern targets are Dockerfile and Planet python client agnostic
+##
+##     "make docker-build-<stage>" to build a stage locally.
+##     "make docker-run-<stage>" to run a stage locally/
+##     "make docker-publish-<stage>" to push a stage a docker repository
+##
+## Options and arguments can be tailored for running each stage in the Dockerfile
+## from make by setting the following in your makefile:
+##     <stage>__DOCKER_RUN_OPTS=
+##     <stage>__DOCKER_RUN_ARGS=
+##
+
+DOCKER_EXE:=docker
+DOCKER_NAME:=$(if $(strip $(DOCKER_NAME)),$(strip $(DOCKER_NAME)),__unset_docker_name__)
+DOCKER_FQNAME:=$(if $(strip $(DOCKER_FQNAME)),$(strip $(DOCKER_FQNAME)),__unset_docker_fqname__)
+DOCKER_VERSION:=$(if $(strip $(DOCKER_VERSION)),$(strip $(DOCKER_VERSION)),__unset_docker_version__)
+DOCKER_TAG_BASE:=$(if $(strip $(DOCKER_TAG_BASE)),$(strip $(DOCKER_TAG_BASE)),$(DOCKER_VERSION))
+
+
+docker-build-%: ## Build and tag the docker stage '%'. The value of '%' is a wildcard, and will be expanded at runtime.
+	$(DOCKER_EXE) build --target "$*" -t "$(DOCKER_NAME):$(DOCKER_TAG_BASE)-$*" .
+
+
+docker-run-%: ## Execute the entrypoint for docker stage '%'. The value of '%' is a wildcard, and will be expanded at runtime.
+docker-run-%: _docker_args=$($*__DOCKER_RUN_OPTS)
+docker-run-%: _prog_args=$($*__DOCKER_RUN_ARGS)
+docker-run-%: docker-build-%
+	$(DOCKER_EXE) run --rm $(_docker_args) "$(DOCKER_NAME):$(DOCKER_TAG_BASE)-$*" $(_prog_args)
+
+
+docker-fqtag-%: ## Tag the local docker image for the stage '%' with fully qualified name and tags suitable for publication to the the container registry. Some of the tags will omit the Dockerfile stage decoration.  WARNING: If you invoke docker-fqtag-% for multiple stages in the docker file, the undecorated tags WILL CLOBBER each other.  You should pay particular attention to how this interacts with the docker publishing targets.
+docker-fqtag-%:  docker-build-%
+	$(DOCKER_EXE) tag  "$(DOCKER_NAME):$(DOCKER_TAG_BASE)-$*" "$(DOCKER_FQNAME):$(DOCKER_TAG_BASE)-$*"
+	$(DOCKER_EXE) tag  "$(DOCKER_NAME):$(DOCKER_TAG_BASE)-$*" "$(DOCKER_FQNAME):$(DOCKER_TAG_BASE)"
+
+
+docker-publish-%: ## Publish the latest docker image for the stage '%' to the docker registry.  WARNING: If multiple stages are publised to the registry, docker tags that omit the stage decorator WILL clobber eachother.
+docker-publish-%: docker-fqtag-%
+	$(DOCKER_EXE) push "$(DOCKER_FQNAME):$(DOCKER_TAG_BASE)-$*"
+	$(DOCKER_EXE) push "$(DOCKER_FQNAME):$(DOCKER_TAG_BASE)"
+
+
+help:  ## Show this help.
+	@fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | grep -v '^#' | sort | sed -e 's/:://' -e 's/://'|  awk -F' *## *' '{printf "%-25s : %s\n", $$1, $$2}'
